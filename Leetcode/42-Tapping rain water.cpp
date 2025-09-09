2fa3423815d113e7a1e385fc09e4efb5309e362c
Ashish Singh <ashish953287@gmail.com>
2025-09-09 20:50:52 +0530

    Create 42-Tapping rain water.cpp

---
+++ Leetcode/42-Tapping rain water.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int l = 0;
+        int r = n-1;
+        int lMax = 0;
+        int rMax = 0;
+        int water = 0;
+        while(l < r){
+            lMax = max(lMax, height[l]);
+            rMax = max(rMax, height[r]);
+            if(lMax <= rMax){
+                water += lMax - height[l];
+                l++; 
+            }else{
+                water += rMax - height[r];
+                r--;
+            }
+        }
+        return water;
+    }
+};
